fec22d6bda887d3a5a5a407952b7cfddbcdc3346
sunxing <1056819708@qq.com>
2022-10-23 11:34:16 +0800

    tidb

---
+++ go.mod
@@ -0,0 +1,26 @@
+module github.com/k3s-io/kine
+
+go 1.16
+
+require (
+	github.com/Rican7/retry v0.1.0
+	github.com/canonical/go-dqlite v1.5.1
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/klauspost/compress v1.14.4
+	github.com/lib/pq v1.10.2
+	github.com/mattn/go-sqlite3 v1.14.15
+	github.com/nats-io/jsm.go v0.0.31-0.20220317133147-fe318f464eee
+	github.com/nats-io/nats.go v1.13.1-0.20220318132711-e0e03e374228
+	github.com/pkg/errors v0.9.1
+	github.com/prometheus/client_golang v1.11.0
+	github.com/rancher/wrangler v0.8.3
+	github.com/shengdoushi/base58 v1.0.0
+	github.com/sirupsen/logrus v1.7.0
+	github.com/soheilhy/cmux v0.1.5
+	github.com/urfave/cli v1.22.4
+	go.etcd.io/etcd/api/v3 v3.5.0
+	go.etcd.io/etcd/client/pkg/v3 v3.5.0
+	go.etcd.io/etcd/client/v3 v3.5.0
+	go.etcd.io/etcd/server/v3 v3.5.0
+	google.golang.org/grpc v1.38.0
+)
---
+++ main.go
@@ -0,0 +1,120 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/k3s-io/kine/pkg/endpoint"
+	"github.com/k3s-io/kine/pkg/metrics"
+	"github.com/k3s-io/kine/pkg/version"
+	"github.com/rancher/wrangler/pkg/signals"
+	"github.com/sirupsen/logrus"
+	"github.com/urfave/cli"
+)
+
+var (
+	config        endpoint.Config
+	metricsConfig metrics.Config
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "kine"
+	app.Usage = "Minimal etcd v3 API to support custom Kubernetes storage engines"
+	app.Version = fmt.Sprintf("%s (%s)", version.Version, version.GitCommit)
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:        "listen-address",
+			Value:       "0.0.0.0:2379",
+			Destination: &config.Listener,
+		},
+		cli.StringFlag{
+			Name:        "endpoint",
+			Usage:       "TIDB",
+			Value:       "tidb://3zyG6KNWGWyhrew.root:sunxing123-=@tcp(gateway01.ap-southeast-1.prod.aws.tidbcloud.com:4000)/test",
+			Destination: &config.Endpoint,
+		},
+		cli.StringFlag{
+			Name:        "ca-file",
+			Usage:       "CA cert for DB connection",
+			Destination: &config.BackendTLSConfig.CAFile,
+		},
+		cli.StringFlag{
+			Name:        "cert-file",
+			Usage:       "Certificate for DB connection",
+			Destination: &config.BackendTLSConfig.CertFile,
+		},
+		cli.StringFlag{
+			Name:        "server-cert-file",
+			Usage:       "Certificate for etcd connection",
+			Destination: &config.ServerTLSConfig.CertFile,
+		},
+		cli.StringFlag{
+			Name:        "server-key-file",
+			Usage:       "Key file for etcd connection",
+			Destination: &config.ServerTLSConfig.KeyFile,
+		},
+		cli.IntFlag{
+			Name:        "datastore-max-idle-connections",
+			Usage:       "Maximum number of idle connections retained by datastore. If value = 0, the system default will be used. If value < 0, idle connections will not be reused.",
+			Destination: &config.ConnectionPoolConfig.MaxIdle,
+			Value:       0,
+		},
+		cli.IntFlag{
+			Name:        "datastore-max-open-connections",
+			Usage:       "Maximum number of open connections used by datastore. If value <= 0, then there is no limit",
+			Destination: &config.ConnectionPoolConfig.MaxOpen,
+			Value:       0,
+		},
+		cli.DurationFlag{
+			Name:        "datastore-connection-max-lifetime",
+			Usage:       "Maximum amount of time a connection may be reused. If value <= 0, then there is no limit.",
+			Destination: &config.ConnectionPoolConfig.MaxLifetime,
+			Value:       0,
+		},
+		cli.StringFlag{
+			Name:        "key-file",
+			Usage:       "Key file for DB connection",
+			Destination: &config.BackendTLSConfig.KeyFile,
+		},
+		cli.StringFlag{
+			Name:        "metrics-bind-address",
+			Usage:       "The address the metric endpoint binds to. Default :8080, set 0 to disable metrics serving.",
+			Destination: &metricsConfig.ServerAddress,
+			Value:       ":8080",
+		},
+		cli.DurationFlag{
+			Name:        "slow-sql-threshold",
+			Usage:       "The duration which SQL executed longer than will be logged. Default 1s, set <= 0 to disable slow SQL log.",
+			Destination: &metrics.SlowSQLThreshold,
+			Value:       time.Second,
+		},
+		cli.BoolFlag{Name: "debug"},
+	}
+	app.Action = run
+
+	if err := app.Run(os.Args); err != nil {
+		if !errors.Is(err, context.Canceled) {
+			logrus.Fatal(err)
+		}
+	}
+}
+
+func run(c *cli.Context) error {
+	if c.Bool("debug") {
+		logrus.SetLevel(logrus.TraceLevel)
+	}
+	ctx := signals.SetupSignalHandler(context.Background())
+	metricsConfig.ServerTLSConfig = config.ServerTLSConfig
+	go metrics.Serve(ctx, metricsConfig)
+	config.MetricsRegisterer = metrics.Registry
+	_, err := endpoint.Listen(ctx, config)
+	if err != nil {
+		return err
+	}
+	<-ctx.Done()
+	return ctx.Err()
+}
